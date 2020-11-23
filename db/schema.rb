0147141c76351808ce50ff83da3ad18fc1fb2b80
sunao1986 <nk@sunao-products.com>
2020-11-23 12:27:52 +0900

    small_chage_fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_080500) do
+ActiveRecord::Schema.define(version: 2020_11_23_014035) do
 
   create_table "arts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 255, null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_080500) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
