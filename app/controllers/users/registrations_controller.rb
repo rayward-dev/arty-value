34345cdba965bdfd1270fa89901bf7751b3a69e6
sunao1986 <nk@sunao-products.com>
2020-11-24 15:01:41 +0900

    css small

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,7 +50,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url, :pc])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image_url, :pc, :introduction])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
